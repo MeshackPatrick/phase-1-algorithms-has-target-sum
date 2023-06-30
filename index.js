1a10f0f44abe5146d1b75e79811931aea16333e7
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-30 14:18:21 +0300

    big o lab

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  /* Write your algorithm here
+  Create a function called hasTargetSum that takes in two parameters: array (an array of integers) and target (the target integer).
+Iterate over each pair of elements i and j in the array.
+Check if the sum of array[i] and array[j] is equal to the target.
+If a pair is found, return true.
+If no pair is found after iterating through all pairs, return false.
+
+  */
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
-
 /* 
   Add your pseudocode here
+  function hasTargetSum(array, target):
+    for i from 0 to length of array - 1:
+        for j from i + 1 to length of array:
+          if array[i] + array[j] equals target:
+                return true
+    return false
+
 */
 
 /*
   Add written explanation of your solution here
+  The outer loop iterates over each element in the array, and the inner loop iterates over the remaining elements starting from the next position. This ensures that we cover all possible pairs of numbers without repeating any pairs.
+
+
 */
 
 // You can run `node index.js` to view these console logs
